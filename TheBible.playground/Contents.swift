699b4defd013ab7fea8b7c2fe627cb1e5cb832d4
Jeff Ripke <jripke74@gmail.com>
2017-12-23 07:48:05 -0800

    added words array

--- TheBible.playground/Contents.swift
+++ TheBible.playground/Contents.swift
@@ -1,1 +1,13 @@
-
+let genesisChapOneVerseOne = "In the beginning God created the heavens and the earth."
+let characterCount = genesisChapOneVerseOne.count
+var words: [String] = []
+var word = ""
+for char in genesisChapOneVerseOne {
+    if char != " " {
+        word += String(char)
+    } else {
+        words.append(word)
+        word = ""
+    }
+}
+print(words)
